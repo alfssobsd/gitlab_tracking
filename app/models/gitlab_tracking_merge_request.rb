86aa62b890114f5f45e5fd752b6d4b03688c6dca
Ihor Harahatyi <ihor.harahatyi@atomcream.com>
2017-10-26 14:55:25 +0300

    update if not exists

--- app/models/gitlab_tracking_merge_request.rb
+++ app/models/gitlab_tracking_merge_request.rb
@@ -6,31 +6,30 @@ class GitlabTrackingMergeRequest < ActiveRecord::Base
   class << self
 
     def parse_merge_request_and_create(issue, merge_request, author, assignee)
-      find_or_create_by(gitlab_id: merge_request['id']) do |gtmr|
-        gtmr.issue = issue
+      gtmr = find_or_create_by(gitlab_id: merge_request['id'])
+      gtmr.issue = issue
 
-        gtmr.source = merge_request['source_branch']
-        gtmr.target = merge_request['target_branch']
-        gtmr.title = merge_request['title']
+      gtmr.source = merge_request['source_branch']
+      gtmr.target = merge_request['target_branch']
+      gtmr.title = merge_request['title']
 
-        gtmr.state = merge_request['state']
-        gtmr.merge_status = merge_request['merge_status']
+      gtmr.state = merge_request['state']
+      gtmr.merge_status = merge_request['merge_status']
 
-        gtmr.gitlab_url = merge_request['url']
+      gtmr.gitlab_url = merge_request['url']
 
-        if author
-          gtmr.author_username = author['username']
-          gtmr.author_name = author['name']
-        end
-
-        if assignee
-          gtmr.assignee_username = assignee['username']
-          gtmr.assignee_name = assignee['name']
-        end
-
-        gtmr.timestamp = Time.parse(merge_request['created_at'])
-        gtmr.save
+      if author
+        gtmr.author_username = author['username']
+        gtmr.author_name = author['name']
       end
+
+      if assignee
+        gtmr.assignee_username = assignee['username']
+        gtmr.assignee_name = assignee['name']
+      end
+
+      gtmr.timestamp = Time.parse(merge_request['created_at'])
+      gtmr.save
     end
   end
 
